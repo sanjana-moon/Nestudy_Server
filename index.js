83a5c6094cf5748dce8915c5cb0726adf464ebff
sanjana-moon <sanjanamoonliya@gmail.com>
2026-05-22 10:50:52 +0600

    Project initiated

---
+++ index.js
@@ -0,0 +1,43 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express')
+const dotenv = require('dotenv')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
