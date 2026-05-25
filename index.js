bb382635c0c664fcc47fe4240dc8e76872d2fb2d
sanjana-moon <sanjanamoonliya@gmail.com>
2026-05-25 16:39:15 +0600

    delete fumctionality completed

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
             res.json(result)
         })
          
+        app.delete('/room/:id', async(req, res) => {
+            const {id} = req.params;
+            const result = await roomCollection.deleteOne({
+                _id: new ObjectId(id)
+            })
+            res.json(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
