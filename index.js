bcc577a20ff4bd2ac1f6abaafe8ee1865bd507ab
sanjana-moon <sanjanamoonliya@gmail.com>
2026-06-01 22:27:49 +0600

    jwt initiated and applied for room details page

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv')
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { userInfo } = require("node:os");
+const { log } = require("node:console");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config()
 
 const app = express()
@@ -23,6 +25,29 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+const JWKS = createRemoteJWKSet(
+    new URL("http://localhost:3000/api/auth/jwks")
+)
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req?.headers.authorization
+    if (!authHeader) {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+    const token = authHeader.split(" ")[1]
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JWKS,)
+        console.log(payload);
+        next()
+    } catch (error) {
+        return res.status(403).json({ message: "Forbidden" })
+    }
+}
 
 async function run() {
     try {
@@ -55,9 +80,12 @@ async function run() {
             res.json(result)
         });
 
-        app.get('/room/:id', async (req, res) => {
+        app.get('/room/:id', verifyToken, async (req, res) => {
             const { id } = req.params
-            const result = await roomCollection.findOne({ _id: new ObjectId(id) })
+            const result = await roomCollection.findOne({
+                _id: new ObjectId(id)
+            })
+
             res.json(result)
         });
 
@@ -155,7 +183,7 @@ async function run() {
             res.json({ message: "Booking cancelled successfully" });
         });
 
-        
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
